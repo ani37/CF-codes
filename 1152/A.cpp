bcf3ba5bc0e22e59355a565600fafedeb91574a4
Aniket Agarwalla <44133710+ani37@users.noreply.github.com>
2022-10-23 01:32:44 +0530

    improve code and corrected output

--- 1152/A.cpp
+++ 1152/A.cpp
@@ -1,59 +1,24 @@
 #include<bits/stdc++.h>
-#define ll          long long
-#define pb          push_back
-#define eb          emplace_back
-#define c(P)        cout<<P<<"\n"
-#define pii         pair<ll,ll>
-#define mi          map<ll,ll>
-#define mii         map<pii,ll>
-#define F           first
-#define S           second
-#define mp(x,y)     make_pair(x,y) 
-#define mem(a,val)  memset(a,val,sizeof(a))
-#define fr(i,a,n)   for(ll i=a;i<n;i++)
-#define frr(i,a,n)  for(ll i=n-1;i>=a;i--)
-
-
-const ll N   =  500005;
-const ll mod = 1e9 + 7;
 
 using namespace std;
 
-
-void solve()
-{
-// string s;
-  ll n,m,k,x=0,y=0,c=0,q,ans=0;
-  cin>>n>>m;
-  ll a[n],b[m];
-  for (ll i = 0; i < n; ++i)
-  {
-  cin>>a[i];
-  if(a[i]%2==0)
-    x++;
-  }
-  for (ll i = 0; i < m; ++i)
-  {
-  cin>>b[i];
-   if(b[i]%2==0)
-    y++;
-  }
-  ans = min(x,(m - y)) + min(y,(n - x));
-  c(ans);
-}
-
-signed main(){
-      #ifndef ONLINE_JUDGE
-      freopen("input.txt", "r", stdin);
-      freopen("output.txt", "w", stdout);
-      #endif
-      ios_base::sync_with_stdio(false);   
-      cin.tie(NULL);
-      ll T=1;
-      //cin>>T;
-      while(T--)
-      {
-      solve();
-      }
-      return 0;
+int main(){
+   
+    int n,m,k,x=0,y=0,c=0,q,ans=0;
+    cin>>n>>m;
+    int a[n],b[m];
+    for (ll i = 0; i < n; ++i)
+    {
+    cin>>a[i];
+    if(a[i]%2==0)
+      x++;
+    }
+    for (ll i = 0; i < m; ++i)
+    {
+     cin>>b[i];
+     if(b[i]%2==0)
+      y++;
+    }
+    ans = min(x,(m - y)) + min(y,(n - x));
+    cout << ans << '\n';
     }
